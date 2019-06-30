d3d12dc3d038c24825374eb7aa74ed6e51266747
Andy Lulham <a.lulham@gmail.com>
2019-06-30 16:18:55 +0100

    WIP first pass at result summary

--- tracker/iati/commands.py
+++ tracker/iati/commands.py
@@ -1,5 +1,6 @@
+import csv
 from os import listdir, makedirs
-from os.path import exists, join
+from os.path import exists, join, isdir
 import shutil
 
 from flask import current_app
@@ -131,3 +132,88 @@ def run_iati_tests(date, refresh):
             utils.run_test(test, publisher, output_filepath,
                            org.test_condition, codelists=codelists,
                            today=snapshot_date)
+
+
+@iati_cli.command('summarize')
+@click.option('--date', default='latest',
+              help='Date of the data to summarize, in YYYY-MM-DD. ' +
+                   'Defaults to most recent.')
+@with_appcontext
+def aggregate_results(date):
+    """Summarize results of IATI data tests."""
+
+    iati_result_path = current_app.config.get('IATI_RESULT_PATH')
+    try:
+        result_dates = listdir(join(iati_result_path))
+        if date == 'latest':
+            result_date = max(result_dates)
+        else:
+            if not exists(join(iati_result_path, date)):
+                raise ValueError
+            result_date = date
+    except ValueError:
+        if date:
+            click.secho(f'Error: No IATI results found for given date ({date}).',
+                        fg='red', err=True)
+        else:
+            click.secho('Error: No IATI results to summarize.', fg='red', err=True)
+        click.echo('Perhaps you need to run tests, using:', err=True)
+        if date:
+            click.echo(f'\n    $ flask iati test --date {date}\n', err=True)
+        else:
+            click.echo('\n    $ flask iati test\n', err=True)
+        raise click.Abort()
+
+    click.echo('Loading tests ...')
+    all_tests = utils.load_tests()
+    current_data_test = utils.load_current_data_test()
+
+    snapshot_result_path = join(iati_result_path, result_date)
+
+    click.echo(f'Summarizing results from IATI data snapshot ({result_date}) ...')
+    publishers = [x for x in listdir(snapshot_result_path)
+                  if isdir(join(snapshot_result_path, x))]
+    for publisher_name in publishers:
+        org = models.Organisation.find(publisher_name)
+        if not org:
+            click.secho(f'Error: Publisher "{publisher_name}" not ' +
+                        f'found in database. Database and XML ' +
+                        f'may be out of sync.',
+                        fg='red', err=True)
+            raise click.Abort()
+        click.echo(f'Summarizing results for organisation: ' +
+                   f'{org.name} ({org.id}) ...')
+        for test in all_tests:
+            result_filepath = join(snapshot_result_path, org.id,
+                                   utils.slugify(test.name) + '.csv')
+            if not exists(result_filepath):
+                continue
+            with open(result_filepath) as handler:
+                current_dataset = None
+                for row in csv.DictReader(handler):
+                    if not current_dataset:
+                        dataset_test_results = {}
+                        current_dataset = row['dataset']
+                    elif current_dataset != row['dataset']:
+                        for hierarchy, scores in dataset_test_results.items():
+                            print('Aggregation: All data')
+                            print(f'Dataset: {current_dataset}')
+                            print(f'Test: {test.name}')
+                            print(f'Publisher: {org.id}')
+                            print(f'Hierarchy: {hierarchy}')
+                            total = sum(scores.values())
+                            score = 100. * scores['pass'] / total if total > 0 else 0
+                            print(f'Score: {score}')
+                            print(f'Total: {total}')
+                        dataset_test_results = {}
+                        current_dataset = row['dataset']
+                    hierarchy = row['hierarchy']
+                    if hierarchy not in dataset_test_results:
+                        dataset_test_results[hierarchy] = {
+                            'pass': 0,
+                            'fail': 0,
+                        }
+                    result = row['result']
+                    if result == 'not relevant':
+                        continue
+                    dataset_test_results[hierarchy][result] += 1
